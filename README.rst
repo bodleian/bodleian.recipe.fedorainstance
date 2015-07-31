264aeae11e13b67f9d4bd4a5dcf3a403ed488964
Chenfu Wang <chenfu.wang@bodleian.ox.ac.uk>
2015-07-31 14:05:44 +0100

    working on tests. save a copy in case of loss of changes

--- README.rst
+++ README.rst
@@ -1,6 +1,9 @@
 Usage
 =======
 
+For fedora 4
+--------------------
+
 .. testcode::
    :hide:
 
@@ -8,30 +11,41 @@ Usage
     >>> import mock
     >>> import shutil
     >>> import os
+
+For fedora 3
+--------------------
+
+.. testcode::
+   :hide:
+
+    >>> # test set up
     >>> file_path = os.getcwd()
-    >>> shutil.copy(os.path.join(file_path, 'tests', 'sample.zip'), '/tmp/test.zip')
+    >>> os.makedirs(os.path.join(file_path, 'parts', 'fedora3instance'))
+    >>> shutil.copy(os.path.join(file_path, 'tests', 'sample.zip'), os.path.join(file_path, 'parts', 'fedora3instance', 'fedora.war'))
     >>> patcher = mock.patch('hexagonit.recipe.download.Download') 
     >>> fake = patcher.start()
     >>> def fake_call(url, md5sum=None, path=None):
     ...     print "Downloading %s" % url
     ...     return '/tmp/test.zip', True
     >>> fake.return_value = fake_call
+    >>> patcher2 = mock.patch('bodleian.recipe.fedorainstance.DEFAULT_JAVA_COMMAND', first="echo /usr/bin/java")
+    >>> patcher2.start()
 
-Here is an example configuration::
+Here is a sample configuration file::
 
     >>> buildcfg = """
     ... [buildout]
-    ... parts = fedorainstance
+    ... parts = fedora3instance
     ... 
-    ... [fedorainstance]
+    ... [fedora3instance]
     ... recipe = bodleian.recipe.fedorainstance
-    ... version = 4
+    ... version = 3
     ... tomcat-home = /tmp/tomcat
-    ... tomcat-url-suffix = fedora
+    ... fedora-url-suffix = fedora
+    ... unpack-war-file = true
     ... """
     >>> with open('buildout.cfg', 'w') as f:
     ...     f.write(buildcfg)
-
 
 Here is what you see::
 
@@ -43,6 +57,7 @@ Here is what you see::
     Downloading https://github.com/fcrepo4/fcrepo4/releases/download/fcrepo-4.2.0/fcrepo-webapp-4.2.0.war
     fedorainstance: Extracting package to /tmp/tomcat/webapps/fedora
 
+
 .. testcode::
    :hide:
 
@@ -51,7 +66,7 @@ Here is what you see::
     >>> print glob.glob("/tmp/tomcat/webapps/fedora/*")
     ['/tmp/tomcat/webapps/fedora/you_have_tested_it']
     >>> shutil.rmtree("/tmp/tomcat")
+    >>> shutil.rmtree("./parts")
     >>> os.unlink("buildout.cfg")
-
-
+    >>> patcher.stop()
 
--- bodleian/recipe/fedorainstance/__init__.py
+++ bodleian/recipe/fedorainstance/__init__.py
@@ -1,11 +1,7 @@
 import logging
 import os
 import zipfile
-import re
-import stat
-import urllib2
 import zc.buildout
-import shutil
 from genshi.template import Context
 from genshi.template import NewTextTemplate
 from genshi.template.base import TemplateSyntaxError
@@ -14,11 +10,34 @@ from genshi.template.eval import UndefinedError
 
 from hexagonit.recipe.download import Recipe as downloadRecipe
 
+# buildout options
+BUILDOUT = 'buildout'
+FIELD_TOMCAT_HOME = 'tomcat-home'
+FIELD_FEDORA_URL_SUFFIX = 'fedora-url-suffix'
+FIELD_FEDORA_VERSION = 'version'
+FIELD_UNPACK_WAR_FILE = 'unpack-war-file'
+FIELD_DESTINATION = 'destination'
+FIELD_URL = 'url'
+
+# internal constants
+DEFAULT_JAVA_COMMAND = '/usr/bin/java'
+DEFAULT_TOMCAT_WEBAPPS_FOLDER_NAME = 'webapps'
+DEFAULT_FEDORA3_INSTALL_PROPERTIES = 'install.properties'
+DEFAULT_FEDORA3_WAR_FILE_FOLDER = 'install'
+DEFAULT_TMP_DIR = '/tmp'
+DEFAULT_FEDORA_WAR_FILE_NAME = 'fedora.war'
+FEDORA3 = '3'
+FEDORA4 = '4'
+
+# messages
+MESSAGE_MISSING_VERSION = "No version specified"
+MESSAGE_MISSING_TOMCAT_HOME = "No tomcat app directory specified"
+MESSAGE_NOT_SUPPORTED_VERSION = "Specified version %s is not supported"
 
 PACKAGES = {
-    "4": "https://github.com/fcrepo4/fcrepo4/releases/download/fcrepo-4.2.0/fcrepo-webapp-4.2.0.war",
+    FEDORA4: "https://github.com/fcrepo4/fcrepo4/releases/download/fcrepo-4.2.0/fcrepo-webapp-4.2.0.war",
     #"4": "http://localhost:8000/fcrepo-webapp-4.2.0.war",
-    "3": "http://10.0.2.15:8000/fcrepo-installer-3.7.0.jar"
+    FEDORA3: "http://10.0.2.15:8000/fcrepo-installer-3.7.0.jar"
     #http://downloads.sourceforge.net/project/fedora-commons/fedora/3.7.0/fcrepo-installer-3.7.0.jar?r=&ts=1424278682&use_mirror=waia"
 }
 
@@ -32,12 +51,12 @@ class Fedora4Worker:
         self.options = options
         self.logger = logger
 
-        options.setdefault('destination',
-                           os.path.join(options['tomcat-home'],
-                                        "webapps",
-                                        options['tomcat-url-suffix']))
+        options.setdefault(FIELD_DESTINATION,
+                           os.path.join(options[FIELD_TOMCAT_HOME],
+                                        DEFAULT_TOMCAT_WEBAPPS_FOLDER_NAME,
+                                        options[FIELD_FEDORA_URL_SUFFIX]))
 
-        self.options['url'] = PACKAGES[options['version']]
+        self.options[FIELD_URL] = PACKAGES[options[FIELD_FEDORA_VERSION]]
         self.download = downloadRecipe(buildout, name, options)
 
     def work(self):
@@ -55,66 +74,74 @@ class Fedora3Worker:
         self.logger = logger
 
         destination = os.path.join( 
-            buildout['buildout']['parts-directory'], 
+            buildout[BUILDOUT]['parts-directory'], 
             self.name)
+        import pdb; pdb.set_trace()
         if not os.path.isdir(destination):
             os.makedirs(destination)
-        options.setdefault('destination', destination) 
+        options.setdefault(FIELD_DESTINATION, destination) 
 
         download_options = {
-            'url': PACKAGES[options['version']],
+            FIELD_URL: PACKAGES[options[FIELD_FEDORA_VERSION]],
             'download-only': 'true',
-            'destination': '/tmp'
+            FIELD_DESTINATION: DEFAULT_TMP_DIR
         }
         self.download = downloadRecipe(buildout, name, download_options)
 
     def _generate_from_template(self, **kwargs):
-        destination = kwargs['destination']
+        output_file = kwargs['output_file']
         source = kwargs['source']
-        name = kwargs['name']
-        output_file = os.path.join(destination, name)
         with open(source, 'r') as template:
             template = NewTextTemplate(template)
 
-        context = Context(name=name,
-                          buildout=self.buildout['buildout'],
+        context = Context(buildout=self.buildout[BUILDOUT],
                           options=self.buildout[self.name],
                           additional=kwargs)
         try:
             output = template.generate(context).render()
         except (TemplateSyntaxError, UndefinedError) as e:
             raise zc.buildout.UserError(
-                'Error in template {0:s}:\n{1:s}'.format(name, e.msg))
+                'Error in template {0:s}:\n{1:s}'.format(output_file, e.msg))
 
         with open(output_file, 'wb') as outfile:
             outfile.write(output.encode('utf8'))
 
-        self.logger.info('Generated file %r.', name)
+        self.logger.info('Generated file %r.', output_file)
 
     def work(self):
         output = self.download.install()
         self._generate_from_template(
-            destination='/tmp',
-            source=self.template_dir,
-            name='install.properties'
+            output_file=self.tmp_install_properties,
+            source=self.install_properties,
         )
-        command = '/usr/bin/java -jar %s /tmp/install.properties' % output[0]
+        command = '%s -jar %s %s' % (DEFAULT_JAVA_COMMAND,
+                                     output[0],
+                                     self.tmp_install_properties)
         os.system(command)
-        if 'unpack-war-file' in self.options and self.options['unpack-war-file'] == 'true':
-            fedora_war = os.path.join(self.options['destination'],
-                                      "install", "fedora.war")
-            tomcat_webapp = os.path.join(self.options['tomcat-home'], 'webapps', self.options['tomcat-url-suffix'])
+        if self.options.get(FIELD_UNPACK_WAR_FILE, '') == 'true':
+            fedora_war = os.path.join(self.options[FIELD_DESTINATION],
+                                      DEFAULT_FEDORA3_WAR_FILE_FOLDER,
+                                      DEFAULT_FEDORA_WAR_FILE_NAME)
+            tomcat_webapp = os.path.join(
+                self.options[FIELD_TOMCAT_HOME],
+                DEFAULT_TOMCAT_WEBAPPS_FOLDER_NAME,
+                self.options[FIELD_FEDORA_URL_SUFFIX])
             self.logger.info('Unpack war file %s to %s', fedora_war, tomcat_webapp)
             with zipfile.ZipFile(fedora_war) as zip_file:
                 zip_file.extractall(tomcat_webapp)
 
     @property
-    def template_dir(self):
-        return os.path.join(os.path.dirname(__file__), 'install.properties')
+    def install_properties(self):
+        return os.path.join(os.path.dirname(__file__),
+                            DEFAULT_FEDORA3_INSTALL_PROPERTIES)
+    @property
+    def tmp_install_properties(self):
+        return os.path.join(DEFAULT_TMP_DIR,
+                            DEFAULT_FEDORA3_INSTALL_PROPERTIES)
 
 WORKER = {
-    "4": Fedora4Worker,
-    "3": Fedora3Worker
+    FEDORA4: Fedora4Worker,
+    FEDORA3: Fedora3Worker
 }
 
 class FedoraRecipe:
@@ -124,19 +151,22 @@ class FedoraRecipe:
         self.options = options
         self.logger = logging.getLogger(self.name)
 
-        if "version" not in options:
-            self.logger.error("No version specified")
-            raise zc.buildout.UserError("No version specified")
+        # validate options
+        if FIELD_FEDORA_VERSION not in options:
+            self.logger.error(MESSAGE_MISSING_VERSION)
+            raise zc.buildout.UserError(MESSAGE_MISSING_VERSION)
 
-        if "tomcat-home" not in options:
-            self.logger.error("No tomcat app directory specified")
-            raise zc.buildout.UserError("No tomcat app directory specified")
+        if FIELD_TOMCAT_HOME not in options:
+            self.logger.error(MESSAGE_MISSING_TOMCAT_HOME)
+            raise zc.buildout.UserError(MESSAGE_MISSING_TOMCAT_HOME)
 
-        if options["version"] not in PACKAGES:
-            self.logger.error("Specified version %s is not supported" % options['version'])
-            raise zc.buildout.UserError("Specified version %s is not supported" % options['version'])
+        if options[FIELD_FEDORA_VERSION] not in PACKAGES:
+            message = MESSAGE_NOT_SUPPORTED_VERSION % options[FIELD_FEDORA_VERSION]
+            self.logger.error(message)
+            raise zc.buildout.UserError(message)
 
-        worker_class = WORKER[options['version']]
+        # choose worker class
+        worker_class = WORKER[options[FIELD_FEDORA_VERSION]]
         self.worker = worker_class(buildout, name, options, self.logger)
 
     def install(self):
