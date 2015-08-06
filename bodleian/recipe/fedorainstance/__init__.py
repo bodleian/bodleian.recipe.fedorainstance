f266a3aa337c0e2afbdddc7ad43c661077ec014d
Chenfu Wang <chenfu.wang@bodleian.ox.ac.uk>
2015-08-06 11:36:37 +0100

    code refactoring

--- bodleian/recipe/fedorainstance/__init__.py
+++ bodleian/recipe/fedorainstance/__init__.py
@@ -1,20 +1,23 @@
-import logging
 import os
-import zipfile
-import zc.buildout
 import re
-
-
+import logging
+import zipfile
+import tempfile
+import zc.buildout
+import ConfigParser
 from hexagonit.recipe.download import Recipe as downloadRecipe
 
 # buildout options
 BUILDOUT = 'buildout'
+FIELD_FEDORA_VERSION = 'version'
 FIELD_TOMCAT_HOME = 'tomcat-home'
 FIELD_FEDORA_URL_SUFFIX = 'fedora-url-suffix'
-FIELD_FEDORA_VERSION = 'version'
 FIELD_UNPACK_WAR_FILE = 'unpack-war-file'
-FIELD_DESTINATION = 'destination'
+FIELD_INSTALL_PROPERTIES = 'install-properties'
 FIELD_URL = 'url'
+
+# internal download recipe options
+FIELD_DESTINATION = 'destination'
 FIELD_DOWNLOAD_ONLY = 'download-only'
 
 # internal constants
@@ -22,9 +25,13 @@ DEFAULT_JAVA_COMMAND = '/usr/bin/java'
 DEFAULT_TOMCAT_WEBAPPS_FOLDER_NAME = 'webapps'
 DEFAULT_FEDORA3_INSTALL_PROPERTIES = 'install.properties'
 DEFAULT_FEDORA3_WAR_FILE_FOLDER = 'install'
-DEFAULT_TMP_DIR = '/tmp'
+DEFAULT_FEDORA_NAME = 'fedora'
 FEDORA3 = '3'
 FEDORA4 = '4'
+
+# configurations
+DEFAULT_CONFIG_INI_FILE = 'recipe_config.ini'
+SECTION_PACKAGES = 'packages'
 
 # messages
 MESSAGE_MISSING_VERSION = "No version specified"
@@ -32,12 +39,8 @@ MESSAGE_MISSING_TOMCAT_HOME = "No tomcat app directory specified"
 MESSAGE_NOT_SUPPORTED_VERSION = "Specified version %s is not supported"
 MESSAGE_SINGLE_WORD = "A single word is expected"
 
-PACKAGES = {
-    FEDORA4: "https://github.com/fcrepo4/fcrepo4/releases/download/fcrepo-4.2.0/fcrepo-webapp-4.2.0.war",
-    FEDORA3: "http://downloads.sourceforge.net/project/fedora-commons/fedora/3.7.0/fcrepo-installer-3.7.0.jar?r=&ts=1424278682&use_mirror=waia"
-}
 
-
+# utility function
 def is_single_word(word):
     """
     test if a string contains single word or not
@@ -45,78 +48,108 @@ def is_single_word(word):
     return re.match(r'\A[\w-]+\Z', word)
 
 
-class Fedora4Worker:
+class FedoraWorker:
     """
-    Install Fedora 4
+    A genuine fedora worker
     """
-    def __init__(self, buildout, name, options, logger):
+    def __init__(self, buildout, name, options, logger, config):
         self.buildout = buildout
         self.name = name
         self.options = options
         self.logger = logger
+        self.config = config
 
+        # it should download something
+        self.download = downloadRecipe(buildout, name,
+                                       self.get_download_options())
+
+    def get_download_options(self):
+        return {}
+
+    def work(self):
+        """
+        it should install fedora
+        """
+        pass
+
+
+class Fedora4Worker(FedoraWorker):
+    """
+    Install Fedora 4
+    """
+    def get_download_options(self):
+        """
+        Instruct download recipe to deploy war file to
+        tomcat webapps directory
+        """
         download_options = {
             FIELD_DESTINATION: os.path.join(
-                options[FIELD_TOMCAT_HOME],
+                self.options[FIELD_TOMCAT_HOME],
                 DEFAULT_TOMCAT_WEBAPPS_FOLDER_NAME,
-                options[FIELD_FEDORA_URL_SUFFIX])
+                self.options[FIELD_FEDORA_URL_SUFFIX])
         }
         if self.options.get(FIELD_URL, None) is None:
-            download_options[FIELD_URL] = PACKAGES[
-                options[FIELD_FEDORA_VERSION]
-            ]
+            download_options[FIELD_URL] = self.config.get(
+                SECTION_PACKAGES,
+                self.options[FIELD_FEDORA_VERSION]
+            )
         else:
             download_options[FIELD_URL] = self.options.get(FIELD_URL)
 
         if self.options.get(FIELD_UNPACK_WAR_FILE, None) != 'true':
             download_options['download-only'] = 'true'
 
-        self.download = downloadRecipe(buildout, name, download_options)
+        return download_options
 
     def work(self):
+        """
+        Go! download it
+        """
         output = self.download.install()
         return [output]
 
 
-class Fedora3Worker:
+class Fedora3Worker(FedoraWorker):
     """
     Install Fedora 3
     """
-    def __init__(self, buildout, name, options, logger):
-        self.buildout = buildout
-        self.name = name
-        self.options = options
-        self.logger = logger
-
+    def get_download_options(self):
+        """
+        Instruct download recipe to get the fedora installer
+        """
         destination = os.path.join(
-            buildout[BUILDOUT]['parts-directory'],
+            self.buildout[BUILDOUT]['parts-directory'],
             self.name)
         if not os.path.isdir(destination):
             os.makedirs(destination)
-        options.setdefault(FIELD_DESTINATION, destination)
+        self.options.setdefault(FIELD_DESTINATION, destination)
 
         download_options = {
             'download-only': 'true',
-            FIELD_DESTINATION: DEFAULT_TMP_DIR
+            FIELD_DESTINATION: tempfile.gettempdir()
         }
         if self.options.get(FIELD_URL, None) is None:
-            download_options[FIELD_URL] = PACKAGES[
-                options[FIELD_FEDORA_VERSION]
-            ]
-
-        self.download = downloadRecipe(buildout, name, download_options)
+            download_options[FIELD_URL] = self.config.get(
+                SECTION_PACKAGES,
+                self.options[FIELD_FEDORA_VERSION]
+            )
+        return download_options
 
     def work(self):
+        """
+        Install fedora to parts and deploy the war file
+        """
         output = self.download.install()
         with open(self.tmp_install_properties, 'w') as f:
-            f.write(self.options['install-properties'])
-        command = '%s -jar %s %s' % (DEFAULT_JAVA_COMMAND,
-                                     output[0],
-                                     self.tmp_install_properties)
+            f.write(self.options[FIELD_INSTALL_PROPERTIES])
+        command = '%s -jar %s %s' % (
+            DEFAULT_JAVA_COMMAND,
+            output[0],
+            self.tmp_install_properties)
         os.system(command)
         if self.options.get(FIELD_UNPACK_WAR_FILE, '') == 'true':
-            default_fedora_war_file_name = ("%s.war"
-                % self.options[FIELD_FEDORA_URL_SUFFIX])
+            default_fedora_war_file_name = (
+                "%s.war" % self.options[FIELD_FEDORA_URL_SUFFIX])
             fedora_war = os.path.join(
                 self.options[FIELD_DESTINATION],
                 DEFAULT_FEDORA3_WAR_FILE_FOLDER,
@@ -133,13 +166,27 @@ class Fedora3Worker:
 
     @property
     def tmp_install_properties(self):
-        return os.path.join(DEFAULT_TMP_DIR,
+        return os.path.join(tempfile.gettempdir(),
                             DEFAULT_FEDORA3_INSTALL_PROPERTIES)
 
+
+# update this worker dictionary to get new ones
 WORKER = {
     FEDORA4: Fedora4Worker,
     FEDORA3: Fedora3Worker
 }
+
+
+class FedoraWorkerFactory:
+    """
+    Worker factory to get a fedora worker per major version
+    """
+    @staticmethod
+    def get_worker(version):
+        if version is None:
+            return None
+        prime_version = version[0]
+        return WORKER.get(prime_version, None)
 
 
 class FedoraRecipe:
@@ -152,33 +199,47 @@ class FedoraRecipe:
         self.options = options
         self.logger = logging.getLogger(self.name)
 
+        self.config = ConfigParser.RawConfigParser()
+        self.config.read(os.path.join(os.path.dirname(__file__),
+                                      DEFAULT_CONFIG_INI_FILE))
+
         # validate options
         if FIELD_FEDORA_VERSION not in options:
-            self.logger.error(MESSAGE_MISSING_VERSION)
-            raise zc.buildout.UserError(MESSAGE_MISSING_VERSION)
+            self._raise_user_error_exception(MESSAGE_MISSING_VERSION)
 
         if FIELD_TOMCAT_HOME not in options:
-            self.logger.error(MESSAGE_MISSING_TOMCAT_HOME)
-            raise zc.buildout.UserError(MESSAGE_MISSING_TOMCAT_HOME)
+            self._raise_user_error_exception(MESSAGE_MISSING_TOMCAT_HOME)
 
         if FIELD_FEDORA_URL_SUFFIX not in options:
-            self.options[FIELD_FEDORA_URL_SUFFIX] = 'fedora'
+            self.options[FIELD_FEDORA_URL_SUFFIX] = DEFAULT_FEDORA_NAME
         else:
             if not is_single_word(self.options[FIELD_FEDORA_URL_SUFFIX]):
-                self.logger.error(MESSAGE_SINGLE_WORD)
-                raise zc.buildout.UserError(MESSAGE_SINGLE_WORD)
+                self._raise_user_error_exception(MESSAGE_SINGLE_WORD)
 
-        if options[FIELD_FEDORA_VERSION] not in PACKAGES:
-            message = (MESSAGE_NOT_SUPPORTED_VERSION 
-                        % options[FIELD_FEDORA_VERSION])
-            self.logger.error(message)
-            raise zc.buildout.UserError(message)
+        if not self.config.has_option(SECTION_PACKAGES,
+                                      options[FIELD_FEDORA_VERSION]):
+            self._raise_not_supported_version_exception()
         if options.get(FIELD_UNPACK_WAR_FILE, None) is None:
             options[FIELD_UNPACK_WAR_FILE] = 'true'
 
         # choose worker class
-        worker_class = WORKER[options[FIELD_FEDORA_VERSION]]
-        self.worker = worker_class(buildout, name, options, self.logger)
+        worker_class = FedoraWorkerFactory.get_worker(
+            options[FIELD_FEDORA_VERSION]
+        )
+        if worker_class is None:
+            self._raise_not_supported_version_exception()
+        self.worker = worker_class(buildout, name, options,
+                                   self.logger, self.config)
+
+    def _raise_user_error_exception(self, message):
+        self.logger.error(message)
+        raise zc.buildout.UserError(message)
+
+    def _raise_not_supported_version_exception(self):
+        message = (
+            MESSAGE_NOT_SUPPORTED_VERSION % self.options[FIELD_FEDORA_VERSION])
+        self.logger.error(message)
+        raise zc.buildout.UserError(message)
 
     def install(self):
         self.worker.work()
